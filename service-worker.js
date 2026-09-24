eed886a784f4d6c7b704695eb5f3bab2b508966e
hassan6322 <alnashriha@almajdouie.org>
2026-09-24 18:29:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "majdouie-risala-v10";
+const CACHE = "majdouie-risala-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", e => {
